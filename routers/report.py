6c68ec210f05c19f4e9e8f44cbbcce97ac1b87bc
Xhyro-v <naufalazharxyz12@gmail.com>
2026-05-11 21:33:48 +0700

    fixing query sorting

--- routers/report.py
+++ routers/report.py
@@ -79,10 +79,15 @@ def post_report(request: Request,
 
 
 @router.get("/report/laporan-saya")
-def inspect_my_report(request:Request,  db: Session = Depends(get_db)):
+def my_report(request:Request,  db: Session = Depends(get_db)):
     username = request.session.get("user")
     role = request.session.get("role")
-    report = db.query(Report).filter(Report.username == username).all()
+
+
+
+    report = db.query(Report).filter(Report.username == username).order_by(Report.id.desc()).all()
+
+
 
     if not report:
         raise HTTPException(status_code=404, detail="Report not found")
